639492d8ad43851c6ca9536116779d8d027d8623
Ningze Wang <ningzewang@gmail.com>
2020-04-22 00:25:57 -0500

    part 7

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-# CSCE636_Deep_learninng-Project
-Course project
-Shake_detectoin is NN which take training videos and test videos
+# CSCE636_Deep_learninng Course project
+Name: Ningze Wang UIN: 827006879
 
 dataset link: https://drive.google.com/open?id=1wOQH_EJnccqGqr1wcwvsLlHnYobnJgzI
 
+Path:
 
 Training set: /csce636/dataset/RGB/train
-Testing set: /csce636/dataset/RGB/test
+Testing set: /csce636/dataset/RGB/self_test
 Saved model: /csce636/dataset/model/my_model.h5
 Json files: /csce636/dataset/json/
+
+Usage:
+After downloading the dataset, please put the test video into path below:
+/csce636/dataset/RGB/test/
+open shake_detect.py, at the beginning of the code, modify the path varaiables.
+Then run ./shake_detect.py
+
 
 
--- shake_detect.py
+++ shake_detect.py
@@ -16,19 +16,23 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # --------------------------------------------
 img_rows, img_cols, num_frames = 320, 180, 120
 
+train_dataset_path = '/home/alex/Desktop/csce636/dataset/RGB/train/'
+test_data_path = '/home/alex/Desktop/csce636/dataset/RGB/test/'
+model_save_path = "/home/alex/Desktop/csce636/dataset/model/my_model.h5"
+json_save_path = "/home/alex/Desktop/csce636/dataset/json/test/"
 # Get the train videos frames into the numpy array
 # output:
 # 1.training group: X_partial_train  X_partial_labels
 # 2.validation group: X_val  val_labels
 # --------------------------------------------
 X_train = []
-listing1 = os.listdir('/home/alex/Desktop/csce636/dataset/RGB/train/')
+listing1 = os.listdir(train_dataset_path)
 listing1.sort()
 listing1.sort(key=lambda x: int(x[:-4]))
 print("Enter training Videos directory")
 
 for vid in listing1:
-    vid = '/home/alex/Desktop/csce636/dataset/RGB/train/' + vid
+    vid = train_dataset_path + vid
     train_frames = []
     cap = cv2.VideoCapture(vid)
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -55,7 +59,9 @@ for vid in listing1:
 print("Training videos reading done")
 
 X_train_array = np.array(X_train)
-
+X_train_array = X_train_array.astype('float32')
+X_train_array -= np.mean(X_train_array)
+X_train_array /= np.max(X_train_array)
 print(X_train_array.shape)
 num_samples = len(X_train_array)
 train_labels = []
@@ -70,8 +76,64 @@ val_labels = train_labels[:60]
 X_partial_labels = train_labels[60:]
 print("train sample numbers: ", num_samples)
 print("train videos shape: ", X_train_array.shape)
-# --------------------------------------------
 
+# --------------------------------------------
+# use the provided video to test
+# output: test_array test_labels
+# --------------------------------------------
+X_test = []
+listing2 = os.listdir(test_data_path)
+listing2.sort()
+print("Enter test videos directory")
+
+for vid in listing2:
+    vid = test_data_path + vid
+    test_frames = []
+    cap = cv2.VideoCapture(vid)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    print(vid, fps, total_frames)
+
+    for k in range(4200):
+        ret, frame = cap.read()
+        if ret:
+            frame = cv2.resize(frame, (img_rows, img_cols), interpolation=cv2.INTER_AREA)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            test_frames.append(gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+    test_input = np.array(test_frames)
+    test_ipt = np.rollaxis(test_input, 2, 1)
+    X_test.append(test_ipt)
+
+print("test videos reading done")
+
+X_test_array = np.array(X_test)
+X_test_array = np.asarray(X_test_array).astype('float32')
+X_test_array = X_test_array.astype('float32')
+X_test_array -= np.mean(X_test_array)
+X_test_array /= np.max(X_test_array)
+
+test_labels = np.zeros(210)
+test_labels = np.asarray(test_labels).astype('float32')
+num_samples = len(X_test_array)
+X_test_array = X_test_array.reshape(210, 20, img_rows, img_cols, 1)
+print("test sample numbers: ", num_samples)
+print("test videos shape: ", X_test_array.shape)
+
+
+
+
+
+
+
+'''
+# --------------------------------------------
 # get the test videos frames into the numpy array
 # output: X_test_array test_labels
 # --------------------------------------------
@@ -109,6 +171,10 @@ print("test videos reading done")
 
 X_test_array = np.array(X_test)
 X_test_array = np.asarray(X_test_array).astype('float32')
+X_test_array = X_test_array.astype('float32')
+X_test_array -= np.mean(X_test_array)
+X_test_array /= np.max(X_test_array)
+
 test_labels = []
 for i in range(5):
     for j in range(6):
@@ -121,6 +187,8 @@ num_samples = len(X_test_array)
 X_test_array = X_test_array.reshape(num_samples * 6, 20, img_rows, img_cols, 1)
 print("test sample numbers: ", num_samples)
 print("test videos shape: ", X_test_array.shape)
+
+'''
 
 '''
 # --------------------------------------------
@@ -159,9 +227,6 @@ print("test_labels shape: ", test_labels.shape)
 # Neural network setting
 # --------------------------------------------
 
-X_train_array = X_train_array.astype('float32')
-X_train_array -= np.mean(X_train_array)
-X_train_array /= np.max(X_train_array)
 
 '''
 model_exist = os.path.exists('')
@@ -173,7 +238,7 @@ else:
  '''
 model = models.Sequential()
 
-model.add(layers.Conv3D(32, (2, 3, 3), strides=(2, 1, 1), padding='same', activation='relu', input_shape=(20, 320, 180, 1)))
+model.add(layers.Conv3D(32, (2, 3, 3), strides=(1, 1, 1), padding='same', activation='relu', input_shape=(20, 320, 180, 1)))
 
 model.add(layers.MaxPooling3D((2, 2, 2)))
 
@@ -195,25 +260,27 @@ model.compile(loss='binary_crossentropy',
 
 history = model.fit(X_partial_train,
                     X_partial_labels,
-                    epochs=8,
-                    batch_size=8,
+                    epochs=2,
+                    batch_size=4,
                     validation_data=(X_val, val_labels))
+
 results = model.predict_classes(X_test_array)
-results = results.reshape((5, 6))
+results = results.reshape((1, 210))
 print(results)
+
 a = model.evaluate(X_test_array, test_labels)
 print(a)
-model.save("/home/alex/Desktop/csce636/dataset/model/my_model.h5")
-for k in range(5):
+model.save(model_save_path)
+for k in range(1):
     json_data = {"Tremor": []}
-    for i in range(6):
+    for i in range(210):
         json_data["Tremor"].append([i, int(results[k][i])])
-    json_path = "/home/alex/Desktop/csce636/dataset/json/test" + str(k) + ".json"
+    json_path = json_save_path + str(k) + ".json"
     with open(json_path, 'w') as f:
         json.dump(json_data, f)
-    time = range(1, 7)
+    time = range(1, 211)
     plt.figure()
     plt.plot(time, results[k], 'bo')
     plt.xlabel("frames")
-    plt.ylabel("class")
+    plt.ylabel("Tremor: yes or no")
     plt.show()
